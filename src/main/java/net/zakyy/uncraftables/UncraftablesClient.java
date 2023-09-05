3790edc2bbc0847024907c14441969e9f97ad918
Zakyy <105586031+Zakyy1@users.noreply.github.com>
2023-09-05 19:45:59 +0200

    Initial commit

---
+++ src/main/java/net/zakyy/uncraftables/UncraftablesClient.java
@@ -0,0 +1,10 @@
+package net.zakyy.uncraftables;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UncraftablesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
